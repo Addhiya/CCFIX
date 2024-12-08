745b4dc0467ff5e8f91ca2f0d537b4b380bdd053
Asady <fayyadh.asady04@gmail.com>
2024-12-08 22:32:16 +0700

    added embedded streamlit n user management part 2

--- app/middleware.tsx
+++ app/middleware.tsx
@@ -1,31 +1,18 @@
-import { useRouter } from 'next/router';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   // Ambil token dari cookie (pastikan Anda sudah menyetel token ke dalam cookie di aplikasi Anda)
   const token = request.cookies.get('authToken');
+  // Daftar halaman publik yang tidak memerlukan login
   const publicPaths = ['/about'];
-  const router = useRouter();
 
   // Cek apakah request URL termasuk halaman publik
   const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
-  if (!token && !isPublicPath) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
-  const adminRoutes = ['/users'];
-  // Daftar halaman publik yang tidak memerlukan login
-  
 
   // Jika tidak ada token dan halaman tidak termasuk halaman publik, redirect ke halaman login
-  
-  const userRole = request.cookies.get('role')?.value;
-
-  if (adminRoutes.includes(request.nextUrl.pathname)) {
-    if (userRole !== 'ADMIN') {
-      router.push('/')
-    }
+  if (!token && !isPublicPath) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   // Jika ada token atau halaman yang diakses adalah halaman publik, lanjutkan request
@@ -36,7 +23,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Proteksi semua halaman kecuali halaman publik dan beberapa folder penting
-      '/users/:path',
     '/((?!api|_next/static|_next/image|about).*)',
   ],
 };
